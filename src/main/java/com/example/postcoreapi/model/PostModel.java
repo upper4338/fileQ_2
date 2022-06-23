66ef4bf5097f0e261be8cf42fc9800097208633d
fw <fw@fw>
2022-06-24 00:07:34 +0600

    Initial commit

---
+++ src/main/java/com/example/postcoreapi/model/PostModel.java
@@ -0,0 +1,24 @@
+package com.example.postcoreapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostModel {
+    @NotNull
+    private String postId;
+
+    @NotNull
+    private String clientId;
+
+    @NotNull
+    private String postRecipientId;
+
+    private String postItem;
+    private String status;
+}
